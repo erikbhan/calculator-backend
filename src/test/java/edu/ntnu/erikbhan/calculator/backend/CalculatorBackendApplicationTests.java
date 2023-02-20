507943a469191c687ef92d5fafcfc8a2ff9a2c31
Erik Borgeteien Hansen <38139009+erikbhan@users.noreply.github.com>
2023-02-20 23:18:50 +0100

    Initial commit

---
+++ src/test/java/edu/ntnu/erikbhan/calculator/backend/CalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.erikbhan.calculator.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
